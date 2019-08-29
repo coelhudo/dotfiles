e732482378e70d1e82de48a7107255f9aa722ba9
coelho <coelhudo@gmail.com>
2019-08-29 02:33:15 -0400

    add conf related to org-agenda

--- emacs/emacs.d/settings/my-org.el
+++ emacs/emacs.d/settings/my-org.el
@@ -1,13 +1,34 @@
 (require 'org)
 (require 'alert)
 (require 'org-ref)
-(setq org-default-notes-file (concat org-directory "/notes.org"))
 
+;;; Code:
 (setq org-latex-pdf-process (list "latexmk -pdf -f %f -output-directory=%o"))
 
 (add-to-list 'alert-user-configuration '(((:category . "org-pomodoro")) libnotify nil))
 
-(setq org-pomodoro-play-sounds nil)
-(global-set-key (kbd "M-p") 'org-pomodoro)
+(use-package org-mode
+  :bind (("C-c l" . org-store-link)
+	 ("C-c a" . org-agenda)
+	 ("C-c c" . org-capture)
+	 ("C-c b" . org-switchb))
+  :config
+  (setq org-agenda-files "~/Dropbox/TODO/gcal.org"))
+
+(setq org-capture-templates
+      '(("a" "Appointment" entry (file "~/Dropbox/TODO/gcal.org")
+	 "* %?\n:PROPERTIES:\n:calendar-id: coelhudo@gmail.com\n:END:\n:org-gcal:\n%^T\n:END:\n")
+	("t" "Todo" entry (file+headline "~/Dropbox/TODO/TODO.org" "Backlog")
+	 "** TODO %?\n%u" :prepend t)
+	("n" "Notes" entry (file "~/Dropbox/TODO/notes.org")
+	 "* %?\n%u" :prepend t)))
+
+
+(use-package org-pomodoro
+  :bind (("M-p" . org-pomodoro))
+  :config
+  (setq org-pomodoro-play-sounds nil))
+
 
 (provide 'my-org)
+;;; my-org ends here
