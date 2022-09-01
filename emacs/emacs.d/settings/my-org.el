36ce6bd5437d39290a7f5e2e9f1234102b7a30a7
coelho <coelhudo@gmail.com>
2022-09-01 02:03:31 -0400

    update my-org.el; remove shortcut from Xresources

--- emacs/emacs.d/settings/my-org.el
+++ emacs/emacs.d/settings/my-org.el
@@ -37,8 +37,14 @@
   (org-roam-complete-everywhere t)
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
-         ("C-c n i" . org-roam-node-insert))
+         ("C-c n i" . org-roam-node-insert)
+         :map org-roam-dailies-map
+         ("Y" . org-roam-dailies-capture-yesterday)
+         ("T" . org-roam-dailies-capture-tomorrow))
+  :bind-keymap
+  ("C-c n d" . org-roam-dailies-map)
   :config
+  (require 'org-roam-dailies)
   (org-roam-db-autosync-enable))
 
 ;;; GTD configuration from here:
